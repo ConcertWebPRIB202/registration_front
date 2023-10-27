14fc04b084cdf0eee930dd35b2fb08539ad2fb0e
MrEsbens <85022303+MrEsbens@users.noreply.github.com>
2023-10-27 05:20:11 +0300

    logo & 5 nonfunctional fields added

--- registration_front/src/App.vue
+++ registration_front/src/App.vue
@@ -1,85 +1,217 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="registration-form-container">
+        <div class="logo">
+            <!-- <router-link to="/"> -->
+            <div class="logo-image">
+                <img src="./assets/LogoShaman.svg">
+            </div>
+            <div class="logo-text">
+                SHAMAN
+            </div>
+            <!-- </router-link> -->
+        </div>
+        <div class="registration-form">
+            <form action="">
+                <div class="obligatory-text-field">
+                    <div class="star">
+                        <img src="./assets/Star.svg">
+                    </div>
+                    <input class="obligatory-text-field-input" type="text" placeholder="Логин">
+                </div>
+                <div class="obligatory-password-field-w-tooltip">
+                    <div class="star">
+                        <img src="./assets/Star.svg">
+                    </div>
+                    <input class="obligatory-password-field-w-tooltip-input" type="password" placeholder="Пароль">
+                    <div class="eye-icon">
+                        <img src="./assets/Eye.svg" alt="">
+                    </div>
+                    <div class="tooltip">
+                        <img src="./assets/Tooltip.svg" alt="">
+                    </div>
+                </div>
+                <div class="obligatory-password-field-wout-tooltip">
+                    <div class="star">
+                        <img src="./assets/Star.svg">
+                    </div>
+                    <input class="obligatory-password-field-wout-tooltip-input" type="password" placeholder="Повторите пароль">
+                    <div class="eye-icon">
+                        <img src="./assets/Eye.svg" alt="">
+                    </div>
+                </div>
+                <div class="obligatory-phone-field">
+                    <div class="star">
+                        <img src="./assets/Star.svg">
+                    </div>
+                    <input class="obligatory-phone-field-input" type="tel" placeholder="+7">
+                    <div class="tooltip">
+                        <img src="./assets/Tooltip.svg" alt="">
+                    </div>
+                </div>
+                <div class="city-field-wout-tooltip">
+                    <input class="city-field-wout-tooltip-input" type="password" placeholder="Город">
+                    <div class="list-icon">
+                        <img src="./assets/List.svg" alt="">
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+    .registration-form-container {
+        display: flex;
+        background-color: #131313;
+        max-width: 1600px;
+        border-radius: 30px;
+        margin-top: 60px;
+        margin-left: auto;
+        margin-right: auto;
+        flex-direction: row;
+        color: #ffffff;
+    }
+    .logo{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+    }
+    .logo-text{
+        font-size: 120px;
+        width:min-content;
+        text-decoration: none;
+    }
+    .registration-form{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin: 60px;
+        align-items: start;
+    }
+    .obligatory-text-field{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .obligatory-text-field-input{
+        background-color: #373737;
+        margin: 8px;
+        height: 61px;
+        width: 413px;
+        border-radius: 30px;
+        border-width: 0px;
+        font-size: 32px;
+    }
+    .obligatory-text-field-input::placeholder{
+        padding-left: 32px;
+        color: #ffffff;
+    }
+    .obligatory-password-field-w-tooltip{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        position: relative;
+    }
+    .obligatory-password-field-w-tooltip-input{
+        background-color: #373737;
+        margin: 8px;
+        height: 61px;
+        width: 413px;
+        border-radius: 30px;
+        border-width: 0px;
+        font-size: 32px;
+    }
+    .obligatory-password-field-w-tooltip-input::placeholder{
+        padding-left: 32px;
+        color: #ffffff;
+    }
+    .obligatory-password-field-w-tooltip .eye-icon{
+        display: flex;
+        align-items: center;
+        position: absolute;
+        right: 55px;
+    }
+    .tooltip{
+        display: flex;
+        align-items: center;
+    }
+    .obligatory-password-field-wout-tooltip{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        position: relative;
+    }
+    .obligatory-password-field-wout-tooltip-input{
+        background-color: #373737;
+        margin: 8px;
+        height: 61px;
+        width: 413px;
+        border-radius: 30px;
+        border-width: 0px;
+        font-size: 32px;
+    }
+    .obligatory-password-field-wout-tooltip-input::placeholder{
+        padding-left: 32px;
+        color: #ffffff;
+    }
+    .obligatory-password-field-wout-tooltip .eye-icon{
+        display: flex;
+        align-items: center;
+        position: absolute;
+        right: 55px;
+    }
+    .obligatory-phone-field{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .obligatory-phone-field-input{
+        background-color: #373737;
+        margin: 8px;
+        height: 61px;
+        width: 413px;
+        border-radius: 30px;
+        border-width: 0px;
+        font-size: 32px;
+    }
+    .obligatory-phone-field-input::placeholder{
+        padding-left: 32px;
+        color: #ffffff;
+    }
+    .city-field-wout-tooltip{
+        margin-top: 20px;
+        margin-left: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        position: relative;
+    }
+    .city-field-wout-tooltip-input{
+        background-color: #373737;
+        margin: 8px;
+        height: 61px;
+        width: 413px;
+        border-radius: 30px;
+        border-width: 0px;
+        font-size: 32px;
+    }
+    .city-field-wout-tooltip-input::placeholder{
+        padding-left: 32px;
+        color: #ffffff;
+    }
+    .city-field-wout-tooltip .list-icon{
+        display: flex;
+        align-items: center;
+        position: absolute;
+        right: 60px;
+    }
 </style>
