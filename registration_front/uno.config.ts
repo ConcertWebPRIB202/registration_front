093fd9f95e491c7ade3778657314ba7eccdd7907
DaHiJl <gmiteor@mail.ru>
2023-11-13 16:37:15 +0300

    Add Front

--- registration_front/uno.config.ts
+++ registration_front/uno.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-
+    rules: [
+        ['main-background-color', {background: '#131313'}],
+        ['submain-background-color', {background: '#565656'}],
+        ['text-color-white', {color: '#FFFFFF'}],
+        ['input-padding', {padding: '12px 32px'}],
+        ['input-margin', {margin: '0 18px'}],
+        ['input-icon-top', {top: '18px'}],
+        ['input-icon-right', {right: '30px'}],
+    ],
 })
